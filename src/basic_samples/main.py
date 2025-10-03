4917a91802e1529faae523126de092df80e5fe0c
Paul Glavich <paulglavich@microsoft.com>
2025-10-03 06:34:42 +0000

    moved into a new dir

--- src/basic_samples/main.py
+++ src/basic_samples/main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 from load_env import load_env
 import asyncio
-from agent_framework import ChatAgent
+from agent_framework import ChatAgent, HostedCodeInterpreterTool
 from agent_framework.azure import AzureAIAgentClient
 from azure.identity.aio import AzureCliCredential
 
@@ -10,6 +10,7 @@ async def main():
     # Load environment variables from .env files
     load_env()
 
+    print("\n.... Generating a joke ......")
     async with (
         AzureCliCredential() as credential,
         ChatAgent(
@@ -19,6 +20,19 @@ async def main():
     ):
         result = await agent.run("Tell me a joke about a pirate.")
         print(result.text)
+
+
+    print("\n.... Using code interpreter ......")
+    async with (
+        AzureCliCredential() as credential,
+        ChatAgent(
+            chat_client=AzureAIAgentClient(async_credential=credential),
+            instructions="You are a helpful assistant that can execute Python code.",
+            tools=HostedCodeInterpreterTool()
+        ) as agent
+    ):
+        response = await agent.run("Calculate the factorial of 100 using Python")
+        print(response.text)
 
 if __name__ == "__main__":
     asyncio.run(main())
